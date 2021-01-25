70e254cb4c3c8990c629373c265df9699544a193
Jiří Vrba <jiri@vrba.dev>
2021-01-25 20:28:08 +0100

    Fix whitespace in name breaking the entire PaperMC

--- src/main/kotlin/cz/vse/fis/minecraft/sortinventory/SortInventory.kt
+++ src/main/kotlin/cz/vse/fis/minecraft/sortinventory/SortInventory.kt
@@ -1,8 +1,16 @@
 package cz.vse.fis.minecraft.sortinventory
 
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
 import org.bukkit.plugin.java.JavaPlugin
 
 class SortInventory : JavaPlugin() {
     override fun onEnable() {
+    }
+
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        println(command)
+        sender.sendMessage("It works!")
+        return true
     }
 }
